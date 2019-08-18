af65ea5ec9fbf6f111a128f8a273059e61577a6e
kev <kevinbilleaud@gmail.com>
2019-08-18 15:11:52 -0600

    marker & infowindow

--- my-googlemap/src/App.js
+++ my-googlemap/src/App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
-import { Map, GoogleApiWrapper } from 'google-maps-react';
+import { Map, GoogleApiWrapper, InfoWindow, Marker } from 'google-maps-react';
+
 
 const mapStyles = {
   width: '100%',
@@ -7,6 +8,28 @@ const mapStyles = {
 }
 
 export class MapContainer extends Component {
+  state = {
+    showingInfoWindow: false,
+    activeMarker: {},
+    selectedPlace: {}
+  };
+
+  onMarkerClick = (props, marker, e) =>
+    this.setState({
+      selectedPlace: props,
+      activeMarker: marker,
+      showingInfoWindow: true
+    });
+
+  onClose = props => {
+    if (this.state.showingInfoWindow) {
+      this.setState({
+        showingInfoWindow: false,
+        activeMarker: null
+      });
+    }
+  };
+
   render() {
     return(
       <Map google={this.props.google}
@@ -16,7 +39,21 @@ export class MapContainer extends Component {
           lat: -1.2884,
           lng: 36.8233
         }}
-      />
+      >
+        <Marker
+          onClick={this.onMarkerClick}
+          name={'Kenyatta International Convention Centre'}
+        />
+        <InfoWindow
+          marker={this.state.activeMarker}
+          visible={this.state.showingInfoWindow}
+          onClose={this.onClose}
+        >
+          <div>
+            <h4>{this.state.selectedPlace.name}</h4>
+          </div>
+        </InfoWindow>
+      </Map>
     );
   }
 }
